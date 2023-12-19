60a63b69f0d480199220bbf51e697e27a8ae5c93
ctrl98 <48410278+ctrl98@users.noreply.github.com>
2023-12-19 10:51:09 +0800

    Update limitInfo.js

--- js/limitInfo.js
+++ js/limitInfo.js
@@ -24,7 +24,7 @@ delete obj.data.freeEquityStatus;
 delete obj.data.msg;
 delete obj.data.remainFreeCount;
 delete obj.data.type;
-obj.data.limit = 0;
+obj.data.limit = flase;
 
 const bodys = JSON.stringify(obj);
 console.log(bodys)
