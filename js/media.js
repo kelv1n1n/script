2ef6c6eaf476a7bda939fe493cb0f420333a7a78
ctrl98 <48410278+ctrl98@users.noreply.github.com>
2023-12-20 14:25:30 +0800

    Update media.js

--- js/media.js
+++ js/media.js
@@ -1,6 +1,15 @@
 let url = $request.url;
 let body = $response.body;
 let obj = JSON.parse(body);
-delete obj.subTypeMap.332;
+
+if ('subTypeMap' in obj) {  
+  for (let key in obj.subTypeMap) {  
+    if (key === '332') {  
+      delete obj.subTypeMap[key];  
+      break; // 如果只需要删除一个，可以使用 break 退出循环  
+    }  
+  }  
+}
+
 body = JSON.stringify(obj);
 $done({body});
