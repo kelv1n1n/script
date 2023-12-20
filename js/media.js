1e91276344700a1d445c53eb35701228a3715e9c
ctrl98 <48410278+ctrl98@users.noreply.github.com>
2023-12-20 16:27:42 +0800

    更新 media.js

--- js/media.js
+++ js/media.js
@@ -10,6 +10,13 @@ if ('subTypeMap' in obj) {
     }  
   }  
 }
-
+if ('subTypeMap' in obj) {  
+  for (let key in obj.subTypeMap) {  
+    if (key === '332') {  
+      console.log(obj.subTypeMap[key]); // 输出键为 '332' 的 JSON 对象  
+      break; // 如果只需要获取一个，可以使用 break 退出循环  
+    }
+  }
+}
 body = JSON.stringify(obj);
 $done({body});
