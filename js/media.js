b6be35c381f263a9d9c0940d4f71bcdff4b0bc84
ctrl98 <48410278+ctrl98@users.noreply.github.com>
2023-12-20 12:48:40 +0800

    更新 media.js

--- js/media.js
+++ js/media.js
@@ -1,19 +1,6 @@
-
-const path = "/chat/limitInfo";
 let url = $request.url;
 let body = $response.body;
-if (url.indexOf(path) != -1) {
-    let obj = JSON.parse(body);
-    console.log(obj);
-    delete obj.data.subMsg;
-    delete obj.data.extMsg;
-    delete obj.data.abValue;
-    delete obj.data.freeEquityStatus;
-    delete obj.data.msg;
-    delete obj.data.remainFreeCount;
-    delete obj.data.type;
-    obj.data.limit = false;
-    body = JSON.stringify(obj);
-    console.log(body)
-}
+let obj = JSON.parse(body);
+delete obj.subTypeMap.332;
+body = JSON.stringify(obj);
 $done({body});
