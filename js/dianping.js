54c6e5026f78f010f1ee77d5e9bb0fb9a18449b7
ctrl98 <48410278+ctrl98@users.noreply.github.com>
2023-12-23 20:47:49 +0800

    更新 dianping.js

--- js/dianping.js
+++ js/dianping.js
@@ -3,7 +3,7 @@ let url = $request.url;
 let body = $response.body;
 let obj = JSON.parse(body);
 
-for (var i = obj.body.bund.length - 1; i >= 0; i--) {
+for (var i = obj.body.bundles.length - 1; i >= 0; i--) {
   if (obj.body.bundles[i].bundleName === "picassovc_pexus-freetry-index_index") {
     console.log('删除了');
     obj.body.bundles.splice(i, 1);
