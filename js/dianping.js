9a8020f886dda2acca124974514a43c0b4579bf8
ctrl98 <48410278+ctrl98@users.noreply.github.com>
2023-12-23 20:20:31 +0800

    更新 dianping.js

--- js/dianping.js
+++ js/dianping.js
@@ -5,7 +5,7 @@ let body = $response.body;
 
 for (var i = body.body.bund.length - 1; i >= 0; i--) {
   if (body.body.bundles[i].bundleName === "picassovc_pexus-freetry-index_index") {
-    body.body.bund.splice(i, 1);
+    body.body.bundles.splice(i, 1);
   }
 }
 
