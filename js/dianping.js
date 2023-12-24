4bfcdc69702600efc464a498f8f3502a57a3e58d
ctrl98 <48410278+ctrl98@users.noreply.github.com>
2023-12-24 13:11:56 +0800

    更新 dianping.js

--- js/dianping.js
+++ js/dianping.js
@@ -1,7 +1,12 @@
-
+const path = 'ddplus.meituan.net'
 let url = $request.url;
 let body = $response.body;
 let obj = JSON.parse(body);
+
+if (url.indexOf(path) {
+    obj = [];
+} else {
+
 obj.body.bundles= [];
 //for (var i = obj.body.bundles.length - 1; i >= 0; i--) {
 //  if (obj.body.bundles[i].bundleName === "picassovc_pexus-freetry-index_index") {
@@ -9,7 +14,7 @@ obj.body.bundles= [];
 //    obj.body.bundles.splice(i, 1);
 //  }
 //}
-
+}
 body = JSON.stringify(obj);
 console.log(body);
 $done({body});
