c486dba1cac81e92aa519a971ce114b40cffac9c
lidahao <48410278+ctrl98@users.noreply.github.com>
2023-12-27 14:46:09 +0800

    update

--- js/Didichuxing.js
+++ js/Didichuxing.js
@@ -20,25 +20,26 @@ if (url.includes("/usercenter/me")) {
   const excludedTitles = ['天天领福利', '金融服务', '更多服务', '企业服务'];
 
   if (obj && obj.data && obj.data.cards) {
+    console.log("cards1:" + obj.data.cards);
     const filteredCards = obj.data.cards.filter(card => !excludedTitles.includes(card.title));
     obj.data.cards = filteredCards;
-
-    if (obj?.data?.cards) {
-      obj.data.cards.forEach(card => {
-        if (card.tag === "wallet") {
-          if (card.items) {
-            const filteredItems = card.items.filter(item => item.title === "优惠券");
-            card.items = filteredItems;
-          }
-          if (card.card_type === 4 && card.bottom_items) {
-            const filteredBottomItems = card.bottom_items.filter(item => 
-              item.title === "省钱套餐" || item.title === "天天神券"
-            );
-            card.bottom_items = filteredBottomItems;
-          }
-        }
-      });
-    }
+    console.log("cards2:" + obj.data.cards);
+    // if (obj?.data?.cards) {
+    //   obj.data.cards.forEach(card => {
+    //     if (card.tag === "wallet") {
+    //       if (card.items) {
+    //         const filteredItems = card.items.filter(item => item.title === "优惠券");
+    //         card.items = filteredItems;
+    //       }
+    //       if (card.card_type === 4 && card.bottom_items) {
+    //         const filteredBottomItems = card.bottom_items.filter(item => 
+    //           item.title === "省钱套餐" || item.title === "天天神券"
+    //         );
+    //         card.bottom_items = filteredBottomItems;
+    //       }
+    //     }
+    //   });
+    // }
   }
 }
 
