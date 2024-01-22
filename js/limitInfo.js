d265670d489ca87189d48487310be86a3283baf6
ctrl98 <48410278+ctrl98@users.noreply.github.com>
2024-01-22 20:58:58 +0800

    Update limitInfo.js

--- js/limitInfo.js
+++ js/limitInfo.js
@@ -20,6 +20,8 @@ if (url.indexOf(path) != -1) {
     obj.data.showRedMind=false;
     obj.data.gameInfo=false;
     obj.data.showLuckyBag=false;
+    obj.data.coreCards.filter(card => card.sortId === 2 || card.sortId === 3);
+
 }
 body = JSON.stringify(obj);
 $done({body});
