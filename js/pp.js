2e80b6efa8c9c8bfe5b05f423962a74ddb0fa649
kelv1n1n <48410278+kelv1n1n@users.noreply.github.com>
2024-01-27 14:55:00 +0800

    Update pp.js

--- js/pp.js
+++ js/pp.js
@@ -3,9 +3,12 @@ let url = $request.url;
 let body = $response.body;
 let obj = JSON.parse(body);
 
-//obj.body.bundles = obj.body.bundles.filter(
-  //card => card.bundleName !== "picassovc_pexus-search-suggest_index"
-//);
+if (url.indexOf("httpdns/resolve") != -1) {
+  obj.data = [];
+  body = JSON.stringify(obj);
+  $done({body});
+}
+
 obj.data = obj.data.filter(item => item.position_type !== 50);
 body = JSON.stringify(obj);
 
