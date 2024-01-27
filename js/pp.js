03fb2345b9958f89c9a42e453c3fc6cc3ec1f415
kelv1n1n <48410278+kelv1n1n@users.noreply.github.com>
2024-01-27 15:41:18 +0800

    Update pp.js

--- js/pp.js
+++ js/pp.js
@@ -3,14 +3,17 @@ let url = $request.url;
 let body = $response.body;
 let obj = JSON.parse(body);
 
-if (url.indexOf("httpdns/resolve") != -1) {
-  obj.data = [];
-  body = JSON.stringify(obj);
-  console.log("ip " + body.data);
-  $done({body});
-}
+//if (url.indexOf("httpdns/resolve") != -1) {
+//  obj.data = [];
+//  body = JSON.stringify(obj);
+//  console.log("ip " + body.data);
+//  $done({body});
+//}
 
-obj.data = obj.data.filter(item => item.position_type !== 50);
+obj.data = obj.data.filter(item => 
+  item.position_type !== 50 || 
+  item.position_type !== 2
+  );
 body = JSON.stringify(obj);
 
 $done({body});
