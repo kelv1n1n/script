1f2c97cfb0385640cfdc1fe7add0016b87e61ba8
kelv1n1n <48410278+kelv1n1n@users.noreply.github.com>
2024-01-27 15:59:45 +0800

    Update pp.js

--- js/pp.js
+++ js/pp.js
@@ -9,7 +9,6 @@ let obj = JSON.parse(body);
 //  console.log("ip " + body.data);
 //  $done({body});
 //}
-
 const typeArr = [50, 2, 810];
 
 obj.data = obj.data.filter(item => !typeArr.includes(item.position_type);
