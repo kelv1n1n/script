8c40f3b43d18fd6fff33d772d427ea6578bf6eda
kelv1n1n <48410278+kelv1n1n@users.noreply.github.com>
2024-01-27 08:33:47 +0800

    Update dianping.js

--- js/dianping.js
+++ js/dianping.js
@@ -1,22 +1,22 @@
-const path = 'ddplus.meituan.net'
+//const path = 'ddplus.meituan.net'
 let url = $request.url;
 let body = $response.body;
 let obj = JSON.parse(body);
 
-if (url.indexOf(path) != -1) {
-    console.log('进来了');
-    obj = [];
-} else {
+//if (url.indexOf(path) != -1) {
+//    console.log('进来了');
+//    obj = [];
+//} else {
 
-obj.body.bundles= [];
+obj.body.bundles= obj.body.bundles.filter
 //for (var i = obj.body.bundles.length - 1; i >= 0; i--) {
 //  if (obj.body.bundles[i].bundleName === "picassovc_pexus-freetry-index_index") {
 //    console.log('删除了');
 //    obj.body.bundles.splice(i, 1);
 //  }
 //}
-}
-console.log(obj);
+//}
+
 body = JSON.stringify(obj);
 console.log(body);
 $done({body});
