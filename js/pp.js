9850af1cec235b87b50ca156c7373d0fe61d63c9
kelv1n1n <48410278+kelv1n1n@users.noreply.github.com>
2024-01-27 18:17:40 +0800

    Update pp.js

--- js/pp.js
+++ js/pp.js
@@ -3,12 +3,13 @@ let url = $request.url;
 let body = $response.body;
 let obj = JSON.parse(body);
 
-//if (url.indexOf("httpdns/resolve") != -1) {
-//  obj.data = [];
-//  body = JSON.stringify(obj);
-//  console.log("ip " + body.data);
-//  $done({body});
-//}
+const mine = "position_types=60%2C560%2C850%2C860%2C890%2C2400";
+if (url.indexOf(mine) != -1) {
+  obj.data = [];
+  body = JSON.stringify(obj);
+  console.log("清除啦");
+  $done({body});
+}
 
 const typeArr = [50, 2, 90, 770];
 obj.data = obj.data.filter(item => !typeArr.includes(item.position_type));
