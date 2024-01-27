a146dc01f03f4f8610ddb019e3373a874ce46d43
kelv1n1n <48410278+kelv1n1n@users.noreply.github.com>
2024-01-27 11:43:19 +0800

    Update dianping.js

--- js/dianping.js
+++ js/dianping.js
@@ -8,19 +8,16 @@ let obj = JSON.parse(body);
 //    obj = [];
 //} else {
 
-obj.body.bundles= obj.body.bundles.filter(
-  card => card.bundleName !== "picassovc_immersive-video_immersivefullscreen" ||
-  card.bundleName !== "picassovc_picasso-attractions-channel_searchlist" ||
-  card.bundleName !== "picassovc_pexus-search-suggest_index"
-  
-);
-//for (var i = obj.body.bundles.length - 1; i >= 0; i--) {
-//  if (obj.body.bundles[i].bundleName === "picassovc_pexus-freetry-index_index") {
-//    console.log('删除了');
-//    obj.body.bundles.splice(i, 1);
-//  }
-//}
-//}
+//obj.body.bundles = obj.body.bundles.filter(
+  //card => card.bundleName !== "picassovc_pexus-search-suggest_index"
+//);
+for (var i = obj.body.bundles.length - 1; i >= 0; i--) {
+  if (obj.body.bundles[i].bundleName === "picassovc_pexus-search-suggest_index") {
+    console.log('删除了');
+    obj.body.bundles.splice(i, 1);
+  }
+}
+}
 
 body = JSON.stringify(obj);
 console.log(body);
