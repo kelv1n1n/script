638f90ae69e48b1538c5be298edc066ca4f1a134
kelv1n1n <48410278+kelv1n1n@users.noreply.github.com>
2024-01-27 13:30:20 +0800

    Update dianping.js

--- js/dianping.js
+++ js/dianping.js
@@ -11,14 +11,14 @@ let obj = JSON.parse(body);
 //obj.body.bundles = obj.body.bundles.filter(
   //card => card.bundleName !== "picassovc_pexus-search-suggest_index"
 //);
-for (var i = obj.body.bundles.length - 1; i >= 0; i--) {
-  if (obj.body.bundles[i].bundleName === "picassovc_pexus-search-suggest_index") {
-    console.log('删除了');
-    obj.body.bundles.splice(i, 1);
-  }
-}
+//for (var i = obj.body.bundles.length - 1; i >= 0; i--) {
+//  if (obj.body.bundles[i].bundleName === "picassovc_pexus-search-suggest_index") {
+//    console.log('删除了');
+//    obj.body.bundles.splice(i, 1);
+//  }
 //}
-//obj.body.bundles = [];
+//}
+obj.body.bundles = [];
 body = JSON.stringify(obj);
 
 $done({body});
