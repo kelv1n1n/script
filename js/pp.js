a6b7e47f1328801479079f0e0206ea7d3c744435
kelv1n1n <48410278+kelv1n1n@users.noreply.github.com>
2024-01-28 09:18:01 +0800

    Update pp.js

--- js/pp.js
+++ js/pp.js
@@ -3,11 +3,19 @@ let url = $request.url;
 let body = $response.body;
 let obj = JSON.parse(body);
 
+const search = "/banner/search_component_banner";
 const mine = "position_types=60%2C560%2C850%2C860%2C890%2C2400";
 if (url.indexOf(mine) != -1) {
   obj.data = [];
   body = JSON.stringify(obj);
   console.log("清除啦");
+  $done({body});
+}
+
+if (url.indexOf(search) != -1) {
+  obj.data = [];
+  body = JSON.stringify(obj);
+  console.log("热搜清除啦");
   $done({body});
 }
 
