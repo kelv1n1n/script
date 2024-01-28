3f5f438c67a9fac410a63fce6c95e625c193c554
kelv1n1n <48410278+kelv1n1n@users.noreply.github.com>
2024-01-28 09:31:30 +0800

    Update pp.js

--- js/pp.js
+++ js/pp.js
@@ -5,6 +5,7 @@ let obj = JSON.parse(body);
 
 const search = "/banner/search_component_banner";
 const mine = "position_types=60%2C560%2C850%2C860%2C890%2C2400";
+const search_hot = "/search/hot_keywords";
 if (url.indexOf(mine) != -1) {
   obj.data = [];
   body = JSON.stringify(obj);
@@ -16,6 +17,13 @@ if (url.indexOf(search) != -1) {
   obj.data = [];
   body = JSON.stringify(obj);
   console.log("热搜清除啦");
+  $done({body});
+}
+
+if (url.indexOf(search_hot) != -1) {
+  obj.data = [];
+  body = JSON.stringify(obj);
+  console.log("热搜发现清除啦");
   $done({body});
 }
 
