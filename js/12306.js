f74021023e5a2e8a293de95ca95536f80815ff54
kelv1n1n <48410278+kelv1n1n@users.noreply.github.com>
2024-02-05 19:17:37 +0800

    Update 12306.js

--- js/12306.js
+++ js/12306.js
@@ -1,1 +1,12 @@
+let body;
+let obj = JSON.parse($request.body);
 
+if (obj.placementNo === "0007") {
+  body = '{"code":"00","message":"无广告返回"}';
+} else if (obj.placementNo === "G0054") {
+  body = '{"code":"00","message":"无广告返回"}';
+} else {
+  body = '{"code":"00","message":"无广告返回"}';
+}
+
+$done({ body });
