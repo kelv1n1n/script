6dc1ff00192387199f893cb18b127f4ef3fd3a3a
kelv1n1n <48410278+kelv1n1n@users.noreply.github.com>
2024-02-05 21:52:01 +0800

    Update 12306.js

--- js/12306.js
+++ js/12306.js
@@ -8,14 +8,9 @@ if (obj.placementNo === "0007") {
     '{"code":"00","materialsList":[{"billMaterialsId":"255","filePath":"h","creativeType":1}],"advertParam":{"skipTime":1}}';
 } else if (obj.placementNo === "G0054") {
   body = '{"code":"00","materialsList":[],"advertParam" : {
-    "fixedscreen" : 3,
-    "displayNumDi" : 1,
     "isDefault" : 0,
     "skipTime" : 1,
-    "chacheTime" : 0,
-    "showSkipBtn" : 0,
-    "skipTimeAgain" : 0,
-    "index" : 3
+    "showSkipBtn" : 0
   }}';
 } else {
   body = '{"code":"00","message":"无广告返回"}';
