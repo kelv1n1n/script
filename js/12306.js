d4dd2cb9d4d829e4604a4ef8dc6f7bb5d3a084f7
kelv1n1n <48410278+kelv1n1n@users.noreply.github.com>
2024-02-05 19:32:25 +0800

    Update 12306.js

--- js/12306.js
+++ js/12306.js
@@ -1,12 +1,7 @@
 let body;
-let obj = JSON.parse($request.body);
+//let obj = JSON.parse($request.body);
+body = JSON.parse($response.body);
 
-if (obj.placementNo === "0007") {
-  body = '{"code":"00","message":"无广告返回"}';
-} else if (obj.placementNo === "G0054") {
-  body = '{"code":"00","message":"无广告返回"}';
-} else {
-  body = '{"code":"00","message":"无广告返回"}';
-}
+body = '{"code":"00","message":"无广告返回"}';
 
 $done({ body });
