ecc05e986aa196a27644435917eba72d05b30a09
kelv1n1n <48410278+kelv1n1n@users.noreply.github.com>
2024-02-05 22:40:41 +0800

    Update 12306.js

--- js/12306.js
+++ js/12306.js
@@ -4,7 +4,7 @@ let obj = JSON.parse($request.body);
 console.log(JSON.stringify(obj));
 console.log("----------");
 if (obj.placementNo === "0007") {
-  body = '{"code":"00","materialsList":[{"billMaterialsId":"255","filePath":"h","creativeType":0}],"advertParam":{"skipTime":1,"showSkipBtn":0}}';
+  body = '{"code":"00","materialsList":[{"billMaterialsId":"255","filePath":"h","creativeType":1}],"advertParam":{"skipTime":1,"showSkipBtn":0}}';
 //body = '{"code":"00","message":"无广告返回"}';
 } else if (obj.placementNo === "G0054") {
   //body = '{"code":"00","materialsList":[],"advertParam":{"skipTime":1,"showSkipBtn":1}}';
