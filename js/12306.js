61c4c780874d04b4949b265cfe012255c6d3aa85
kelv1n1n <48410278+kelv1n1n@users.noreply.github.com>
2024-02-05 22:05:54 +0800

    Update 12306.js

--- js/12306.js
+++ js/12306.js
@@ -1,11 +1,11 @@
 let body;
 let obj = JSON.parse($request.body);
 
-console.log(obj);
+console.log(JSON.stringify(obj));
 console.log("----------");
 if (obj.placementNo === "0007") {
   body =
-    '{"code":"00","materialsList":[{"billMaterialsId":"255","filePath":"h","creativeType":1}],"advertParam":{"skipTime":1}}';
+    '{"code":"00","materialsList":[{"billMaterialsId":"255","filePath":"h","creativeType":1}],"advertParam":{"skipTime":1,"showSkipBtn":0}}';
 } else if (obj.placementNo === "G0054") {
   body = '{"code":"00","materialsList":[],"advertParam":{"skipTime":1,"showSkipBtn":0}}';
 } else {
