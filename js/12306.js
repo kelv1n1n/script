b7b40408fa5ab65a7403b9c07d127ce9534b17cb
kelv1n1n <48410278+kelv1n1n@users.noreply.github.com>
2024-02-05 19:49:57 +0800

    Update 12306.js

--- js/12306.js
+++ js/12306.js
@@ -1,7 +1,23 @@
 let body;
-//let obj = JSON.parse($request.body);
-body = JSON.parse($response.body);
+let obj = JSON.parse($request.body);
 
-body = '{"code":"00","message":"无广告返回"}';
+
+if (obj.placementNo === "0007") {
+  body =
+    '{"code":"00","materialsList":[{"billMaterialsId":"255","filePath":"h","creativeType":1}],"advertParam":{"skipTime":1}}';
+} else if (obj.placementNo === "G0054") {
+  body = '{"code":"00","materialsList":[],"advertParam" : {
+    "fixedscreen" : 3,
+    "displayNumDi" : 1,
+    "isDefault" : 0,
+    "skipTime" : 1,
+    "chacheTime" : 0,
+    "showSkipBtn" : 0,
+    "skipTimeAgain" : 0,
+    "index" : 3
+  }}';
+} else {
+  body = '{"code":"00","message":"无广告返回"}';
+}
 
 $done({ body });
