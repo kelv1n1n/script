b8aa65a54dc1c5c9e63d6d65550417e0d4295cd0
kelv1n1n <48410278+kelv1n1n@users.noreply.github.com>
2024-03-04 16:27:02 +0800

    Update tandem.js

--- js/tandem.js
+++ js/tandem.js
@@ -1,1 +1,14 @@
+const path = "/chat/limitInfo";
 
+let url = $request.url;
+
+let body = $response.body;
+let obj = JSON.parse(body);
+if (url.indexOf(path) != -1) {
+    
+    //body = JSON.stringify(obj);
+    //console.log(body)
+} 
+
+body = JSON.stringify(obj);
+$done({body});
