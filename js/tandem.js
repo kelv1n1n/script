31ea04e720334a617638bb960f86f167960a30e8
kelv1n1n <48410278+kelv1n1n@users.noreply.github.com>
2024-03-04 17:23:03 +0800

    Update tandem.js

--- js/tandem.js
+++ js/tandem.js
@@ -6,10 +6,12 @@ let url = $request.url;
 let body = $response.body;
 let obj = JSON.parse(body);
 if (url.indexOf(path) != -1) {
-    obj.settings.nofill_urls = [];
-    obj.ad_networks = [];
+    //obj.settings.nofill_urls = [];
+    //obj.ad_networks = [];
     //body = JSON.stringify(obj);
     //console.log(body)
+
+    obj.ad_networks[0].ad.enable_omid = false
 } else if (url.indexOf(config) != -1) {
     //obj.ad_unit_settings = [];
     //obj.initializer_settings = {};
