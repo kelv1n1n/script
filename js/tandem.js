263291b0231cb628f826047be7d9be6976daf9f2
kelv1n1n <48410278+kelv1n1n@users.noreply.github.com>
2024-03-04 16:30:54 +0800

    Update tandem.js

--- js/tandem.js
+++ js/tandem.js
@@ -1,11 +1,12 @@
-const path = "/chat/limitInfo";
+const path = "/mads/gma";
 
 let url = $request.url;
 
 let body = $response.body;
 let obj = JSON.parse(body);
 if (url.indexOf(path) != -1) {
-    
+    obj.settings.nofill_urls = [];
+    obj.ad_networks = [];
     //body = JSON.stringify(obj);
     //console.log(body)
 } 
