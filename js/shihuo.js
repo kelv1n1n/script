aebaa1ead4e8c24eab244fa272be17c38f9268e5
kelv1n1n <48410278+kelv1n1n@users.noreply.github.com>
2024-03-05 10:36:45 +0800

    Update shihuo.js

--- js/shihuo.js
+++ js/shihuo.js
@@ -9,7 +9,7 @@ if (url.indexOf(home) != -1) {
     obj.data.new_activity = [];
     obj.data.search_arr = {};
     obj.data.hotspot = {};
-    obj.data.activity_dynamic_modules[0].height = 0
+    obj.data.activity_dynamic_modules = [];
 } else if (url.indexOf(config) != -1) {
 
 }
