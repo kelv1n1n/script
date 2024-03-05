ea3551e45bd01ac7c02814883ee1f4552ad897ce
kelv1n1n <48410278+kelv1n1n@users.noreply.github.com>
2024-03-05 10:24:38 +0800

    Update shihuo.js

--- js/shihuo.js
+++ js/shihuo.js
@@ -1,12 +1,12 @@
-const path = "/mads/gma";
+const home = "/home/info/v1";
 const config = "/getconfig/pubsetting";
 
 let url = $request.url;
 
 let body = $response.body;
 let obj = JSON.parse(body);
-if (url.indexOf(path) != -1) {
-
+if (url.indexOf(home) != -1) {
+    obj.data.new_activity = [];
 } else if (url.indexOf(config) != -1) {
 
 }
