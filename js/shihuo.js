a3e91de94f2d595f1a5922702e55c8426ad5412e
kelv1n1n <48410278+kelv1n1n@users.noreply.github.com>
2024-03-05 12:20:15 +0800

    Update shihuo.js

--- js/shihuo.js
+++ js/shihuo.js
@@ -1,5 +1,6 @@
 const home = "/home/info/v1";
 const tab = "/sh-appapi/app3/saveAppInfo";
+const my = "/sh-appapi/my/config_list";
 
 let url = $request.url;
 
@@ -12,8 +13,15 @@ if (url.indexOf(home) != -1) {
     obj.data.activity_dynamic_modules = [];
 } else if (url.indexOf(tab) != -1) {
     obj.data.tab_icon_go = obj.data.tab_icon_go.filter(item => item.key !== "tab2");
-    console.log(JSON.stringify(obj.data.tab_icon_go));
+    //console.log(JSON.stringify(obj.data.tab_icon_go));
+} else if (url.indexOf(my) != -1) {
+    const arrayKey = ["xiaoyuanhuodong", "wenjuandiaoyan", "qianggou", "kanjia"];
+    obj.data.list.forEach(obj => {  
+      if (obj.key === "wo_de_shihuo") {  
+        obj.list = obj.list.filter(item => !arrayKey.includes(item.key));  
+      }  
+    });  
 }
-console.log(JSON.stringify(obj));
+//console.log(JSON.stringify(obj));
 body = JSON.stringify(obj);
 $done({body});
