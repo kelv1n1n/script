af6b81c67792ce34781d9e175ccbcba07b61b328
kelv1n1n <48410278+kelv1n1n@users.noreply.github.com>
2024-03-06 20:10:31 +0800

    Update keep.js

--- js/keep.js
+++ js/keep.js
@@ -3,6 +3,7 @@ const config = "/config/v3/basic";
 const ad = "/op-engine-webapp/v1/ad";
 const tool = "/tool/v1/jump/popup";
 const search_tab = "/hotHashtag/list";
+const configs = "/v1/configs"
 
 let url = $request.url;
 
@@ -23,6 +24,9 @@ if (url.indexOf(config) != -1) {
   obj.data[0].adResponse = {};
   obj.data[0].schema = "";
   console.log(JSON.stringify(obj.data));
+} else if (url.indexOf(configs) != -1) {
+  obj.data = {};
+  console.log("进来");
 }
 //console.log(JSON.stringify(obj));
 body = JSON.stringify(obj);
