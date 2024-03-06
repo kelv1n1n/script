564a46d3865022ecf27424f2e993f9ca2d35e28a
kelv1n1n <48410278+kelv1n1n@users.noreply.github.com>
2024-03-06 09:36:01 +0800

    Update keep.js

--- js/keep.js
+++ js/keep.js
@@ -1,5 +1,6 @@
 
 const config = "/config/v3/basic";
+const ad = "/op-engine-webapp/v1/ad";
 
 let url = $request.url;
 
@@ -9,8 +10,8 @@ if (url.indexOf(config) != -1) {
   obj.data.generalConfigs['AD.link.download.source'] = "false";
   obj.data.generalConfigs['splash.ad.load.max.timeout'] = "0";
   obj.data.generalConfigs['screen.ks.ad.enabled'] = "false";
-} else if (url.indexOf(tab) != -1) {
-
+} else if (url.indexOf(ad) != -1) {
+  obj.data.hasAd = 0;
 } else if (url.indexOf(my) != -1) {
 
 } else if (url.indexOf(info) != -1) {
