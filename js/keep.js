9f18858f010d97343a262d6bfbb7adb99d829d53
kelv1n1n <48410278+kelv1n1n@users.noreply.github.com>
2024-03-07 09:49:57 +0800

    Update keep.js

--- js/keep.js
+++ js/keep.js
@@ -14,8 +14,11 @@ if (url.indexOf(config) != -1) {
   obj.data.generalConfigs['splash.ad.load.max.timeout'] = "0";
   obj.data.generalConfigs['screen.ks.ad.enabled'] = "false";
 } else if (url.indexOf(ad) != -1) {
-  obj.data.creative = {};
+  //obj.data.creative = {};
+  obj.data.creative.material.fullScreenDisplay = false;
   obj.data.hasAd = 0;
+  console.log(JSON.stringify(obj.data.creative.material));
+  console.log(JSON.stringify(obj.data.hasAd));
 } else if (url.indexOf(tool) != -1) {
   obj.data = {};
 } else if (url.indexOf(search_tab) != -1) {
