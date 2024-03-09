87a12c4a44010926adff78326c4699cdde93f37a
kelv1n1n <48410278+kelv1n1n@users.noreply.github.com>
2024-03-09 09:05:53 +0800

    Update jingdong.js

--- js/jingdong.js
+++ js/jingdong.js
@@ -198,7 +198,7 @@ if (url.includes("functionId=deliverLayer") || url.includes("functionId=orderTra
   obj.result.navigationOrder = "index,cart,home";
   obj.result.modeMap.dark.navigationAll = obj.result.modeMap.dark.navigationAll.filter(item => item.functionId !== "find" || item.functionId !== "new");
   obj.result.modeMap.paramValues = "Index_Cart_Home"
-  console.log(JSON.stringify(obj.result.navigationOrder));
+  console.log(JSON.stringify(obj.result));
 }
 
 $done({ body: JSON.stringify(obj) });
