f87a627e05bca2a3f4bb15b1ef93b080129f4779
kelv1n1n <48410278+kelv1n1n@users.noreply.github.com>
2024-03-11 18:59:38 +0800

    Update soul.js

--- js/soul.js
+++ js/soul.js
@@ -67,11 +67,13 @@ if (url.indexOf(path) != -1) {
     obj.data.positionContentRespList = [];
     //console.log(JSON.stringify(obj.data.positionContentRespList));
 } else if (url.indexOf(bubble) != -1) {
+    console.log("进来了");
     let resp = "";
     resp = '{"code":10001,"message":"success","data":{"mood":"https://china-img.soulapp.cn/admin/2023-02-20/b57cb774-3ef7-4743-be43-9723e4508c38.png","unreadQuantity":0},"success":true}';
+    
     $done({resp});
 }
 
-
+console.log("进来了..");
 body = JSON.stringify(obj);
 $done({body});
