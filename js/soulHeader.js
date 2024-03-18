8a9f6ba647b483096cebcdcf7d0a5c7b492a1cfb
kelv1n1n <48410278+kelv1n1n@users.noreply.github.com>
2024-03-18 17:40:38 +0800

    Update soulHeader.js

--- js/soulHeader.js
+++ js/soulHeader.js
@@ -6,4 +6,4 @@ for (const headerField in req_headers) {
   console.log(`${headerField}: ${req_headers[headerField]}`);
 }
 console.log("遍历头部对象并打印每个字段和值结束🍓");
-$done({ request: { header } });
+$done();
