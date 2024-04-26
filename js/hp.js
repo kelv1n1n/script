3329349074149611f13779f8faa9d56830b3f0c2
kelv1n1n <48410278+kelv1n1n@users.noreply.github.com>
2024-04-26 22:39:34 +0800

    Update hp.js

--- js/hp.js
+++ js/hp.js
@@ -1,15 +1,24 @@
+const init = "/init";
+const topics = "topics";
+
+let url = $request.url;
+
 let body = $response.body;
 let obj = JSON.parse(body);
 
-obj.result.clientLevelOneNavV2 = obj.result.clientLevelOneNavV2.filter(item => item.en === "hotRank" || item.en === "match");
-
-obj.result.clientLevelOneNavV2.forEach(card => {
-        if (card.en === "match") {
-          card.night = {};
-          card.day = {};
-        }
-    });
-
+if (url.indexOf(init) != -1 {
+        
+        obj.result.clientLevelOneNavV2 = obj.result.clientLevelOneNavV2.filter(item => item.en === "hotRank" || item.en === "match");
+        obj.result.clientLevelOneNavV2.forEach(card => {
+              if (card.en === "match") {
+               card.night = {};
+               card.day = {};
+              }
+        });
+}
+if (url.indexOf(topics) != -1 {
+        obj.data.topicResources = [];
+}
 body = JSON.stringify(obj);
 
 $done({body});
