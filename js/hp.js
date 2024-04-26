d4da34d32a31c1b78d1acb05d7b769d7debb3044
kelv1n1n <48410278+kelv1n1n@users.noreply.github.com>
2024-04-26 22:07:59 +0800

    Update hp.js

--- js/hp.js
+++ js/hp.js
@@ -1,7 +1,8 @@
 let body = $response.body;
 let obj = JSON.parse(body);
 
-obj.result.clientLevelOneNavV2 = obj.result.clientLevelOneNavV2.filter(item => item.en != "recommend");
+obj.result.clientLevelOneNavV2 = obj.result.clientLevelOneNavV2.filter(item => item.en === "hotRank" || itme.en === "match");
 
 body = JSON.stringify(obj);
+console.log(body.result.clientLevelOneNavV2);
 $done({body});
