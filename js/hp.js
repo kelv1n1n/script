078af87f5785e86d4c0c430734d6cceee6a1adbf
kelv1n1n <48410278+kelv1n1n@users.noreply.github.com>
2024-04-26 22:01:30 +0800

    Update hp.js

--- js/hp.js
+++ js/hp.js
@@ -1,7 +1,7 @@
 let body = $response.body;
 let obj = JSON.parse(body);
 
-obj.result.clientLevelOneNavV2 = obj.result.clientLevelOneNavV2.filter(item -> item.en != "recommend");
+obj.result.clientLevelOneNavV2 = obj.result.clientLevelOneNavV2.filter(item => item.en != "recommend");
 
 body = JSON.stringify(obj);
 $done({body});
