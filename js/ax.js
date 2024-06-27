55a1eb66bc6f900f4a725f1ab4e23e7e328c40ff
kelv1n1n <48410278+kelv1n1n@users.noreply.github.com>
2024-06-27 23:29:53 +0800

    Update ax.js

--- js/ax.js
+++ js/ax.js
@@ -1,1 +1,11 @@
 
+//const people = "/athena/v7/people/my";
+
+let url = $request.url;
+
+let body = $response.body;
+let obj = JSON.parse(body);
+obj.msg = "";
+
+body = JSON.stringify(obj);
+$done({body});
