82d766438b454166b41e47c5b283bda476bc4127
kelv1n1n <48410278+kelv1n1n@users.noreply.github.com>
2024-07-15 10:11:35 +0800

    Update bigo.js

--- js/bigo.js
+++ js/bigo.js
@@ -5,7 +5,11 @@ let body = $response.body;
 let obj = JSON.parse(body);
 
 if (url.indexOf(home) != -1) {
-    obj.roominfo_vec
+    obj.roominfo_vec.forEach(card => {
+        if (card.room_id === 0) {
+            delete card.banner_data;
+        }
+    });
 }
 
 body = JSON.stringify(obj);
