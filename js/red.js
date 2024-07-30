55f23353035ddce748d7b40d1f770d2b91c64b69
kelv1n1n <48410278+kelv1n1n@users.noreply.github.com>
2024-07-30 18:44:59 +0800

    Update red.js

--- js/red.js
+++ js/red.js
@@ -13,6 +13,15 @@ if (url.indexOf(all) != -1) {
 if (url.indexOf(vip) != -1) {
     delete obj.data.adlist;
     delete obj.data.banner;
+
+    obj.data.list.forEach(card => {
+        card.limit.prerequisite = 0;
+        card.limit.ptid = "0";
+        card.private = 0;
+        delete card.emceelevel;
+        console.log("修改了);
+    });
+    
 }
 
 body = JSON.stringify(obj);
