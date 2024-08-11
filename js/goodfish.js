10bcc66ae8925ac7e2606d7600e50346862a3982
kelv1n1n <48410278+kelv1n1n@users.noreply.github.com>
2024-08-11 09:18:39 +0800

    Update goodfish.js

--- js/goodfish.js
+++ js/goodfish.js
@@ -1,1 +1,12 @@
+const user = "/mtop.idle.user.page.my.adapter";
+let url = $request.url;
 
+let body = $response.body;
+let obj = JSON.parse(body);
+
+if (url.indexOf(user) != -1) {
+  obj.data.container.sections = obj.data.container.sections.filter(item => item.index !== "6");
+}
+
+body = JSON.stringify(obj);
+$done({body});
