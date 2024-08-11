172abb5a347241bc8ba0c226832685fbd81452c6
kelv1n1n <48410278+kelv1n1n@users.noreply.github.com>
2024-08-11 09:36:02 +0800

    Update goodfish.js

--- js/goodfish.js
+++ js/goodfish.js
@@ -11,7 +11,19 @@ if (url.indexOf(user) != -1) {
     if (section.index === "1") {
         delete section.item.level;
     }
-});
+  });
+
+  obj.data.container.sections.forEach(sec => {
+    if (sec.index === "5") {
+        let tools = sec.item.tool.exContent.tools;
+
+        // 删除第一个数组中 toolId 为 14 的元素
+        tools[0] = tools[0].filter(tool => tool.toolId !== 14);
+
+        // 删除第二个数组中 toolId 为 2 的元素
+        tools[1] = tools[1].filter(tool => tool.toolId !== 2);
+    }
+  });
 
 }
 
