86ba89c27e19bc50b9784d4d024fd133de3dc7a7
kelv1n1n <48410278+kelv1n1n@users.noreply.github.com>
2024-08-11 09:26:34 +0800

    Update goodfish.js

--- js/goodfish.js
+++ js/goodfish.js
@@ -6,6 +6,13 @@ let obj = JSON.parse(body);
 
 if (url.indexOf(user) != -1) {
   obj.data.container.sections = obj.data.container.sections.filter(item => item.index !== "6");
+
+  obj.data.container.sections.forEach(section => {
+    if (section.index === "1") {
+        delete section.item.level;
+    }
+});
+
 }
 
 body = JSON.stringify(obj);
