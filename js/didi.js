1b594d65f078d201973bf911f4915baffdbeeab8
kelv1n1n <48410278+kelv1n1n@users.noreply.github.com>
2024-08-20 12:11:42 +0800

    Update didi.js

--- js/didi.js
+++ js/didi.js
@@ -1,10 +1,16 @@
+const url = $request.url;
+if (!$response.body) $done({});
+
+let obj = JSON.parse($response.body);
+
 if (url.includes("/homepage/v1/core")) {
-  const keepNavIds = ['dache_anycar', 'driverservice', 'bike'];
   if (obj.data && obj.data.order_cards && obj.data.order_cards.nav_list_card && obj.data.order_cards.nav_list_card.data) {
-    obj.data.order_cards.nav_list_card.data = obj.data.order_cards.nav_list_card.data.filter(item => keepNavIds.includes(item.nav_id));
+    obj.data.order_cards.nav_list_card.data = [];
   }
   const keepBottomNavIds = ['v6x_home', 'home_page', 'user_center'];
   if (obj.data && obj.data.disorder_cards && obj.data.disorder_cards.bottom_nav_list && obj.data.disorder_cards.bottom_nav_list.data) {
     obj.data.disorder_cards.bottom_nav_list.data = obj.data.disorder_cards.bottom_nav_list.data.filter(item => keepBottomNavIds.includes(item.id));
   }
 }
+
+$done({ body: JSON.stringify(obj) });
