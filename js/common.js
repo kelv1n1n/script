a997f08e5b42f33005dd0186784150d2a0c7fecd
kelv1n1n <48410278+kelv1n1n@users.noreply.github.com>
2024-08-23 14:20:18 +0800

    Update common.js

--- js/common.js
+++ js/common.js
@@ -16,4 +16,30 @@ if (url.includes("/homepage/v1/core")) {
   }
 }
 
+if (url.indexOf(user) != -1) {
+  //  底部社区小程序列表
+  //obj.data.container.sections = obj.data.container.sections.filter(item => item.index !== "6");
+  obj.data.container.sections.forEach(sec => {
+    if (sec.index === "6") {
+        delete sec.item.bottom;
+        delete sec.item.right;
+        delete sec.item.middle;
+        delete sec.item.corner;
+    }
+  });
+  //  个人主页横幅
+  obj.data.container.sections = obj.data.container.sections.filter(item => item.index !== "3");
+
+  //  个人等级
+  obj.data.container.sections.forEach(section => {
+    if (section.index === "1") {
+        delete section.item.level;
+    }
+  });
+
+  //  处理简历菜单
+  obj.data.container.sections = obj.data.container.sections.filter(item => item.index !== "5");
+
+}
+
 $done({ body: JSON.stringify(obj) });
