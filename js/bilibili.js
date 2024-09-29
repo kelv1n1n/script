1dcc5d58fa6f5f5726665afc66b2ae8f84694813
kelv1n1n <48410278+kelv1n1n@users.noreply.github.com>
2024-09-29 09:45:21 +0800

    Update bilibili.js

--- js/bilibili.js
+++ js/bilibili.js
@@ -4,7 +4,21 @@ let obj = JSON.parse(body);
 
 const index = "/feed/index";
 if (url.indexOf(index) != -1) {
-  obj.data.items = obj.data.items.filter((i) => i?.card_goto === "av");
+  //obj.data.items = obj.data.items.filter((i) => i?.card_goto === "av");
+  let newItems = [];
+    for (let item of obj.data.items) {
+      if (item?.hasOwnProperty("ad_info")) {
+        continue;
+      } else if (["vertical_ad_av", "vertical_live", "vertical_pgc"]?.includes(item?.card_goto)) {
+        continue;
+      } else {
+        if (item?.story_cart_icon) {
+          delete item.story_cart_icon;
+        }
+        newItems.push(item);
+      }
+    }
+    obj.data.items = newItems;
 }
 
   
