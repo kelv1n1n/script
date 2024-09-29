fbed52e46d83359d25093f72a24fd58a738120a0
kelv1n1n <48410278+kelv1n1n@users.noreply.github.com>
2024-09-29 10:24:27 +0800

    Update bilibili.js

--- js/bilibili.js
+++ js/bilibili.js
@@ -2,9 +2,17 @@ let url = $request.url;
 let body = $response.body;
 let obj = JSON.parse(body);
 
-const index = "/feed/index";
-if (url.indexOf(index) != -1) {
-  //obj.data.items = obj.data.items.filter((i) => i?.card_goto === "av");
+const index = "/feed/index?";
+if (url.includes(index) {
+  // 首页推荐信息流
+  if (obj?.data?.items?.length > 0) {
+    // 白名单
+    obj.data.items = obj.data.items.filter((i) => i?.card_goto === "av");
+  }
+}
+const index_story = "/feed/index/story";
+if (url.indexOf(index_story) != -1) {
+  // 竖屏模式信息流
   let newItems = [];
     for (let item of obj.data.items) {
       if (item?.hasOwnProperty("ad_info")) {
