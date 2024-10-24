f4c731db670ca7e85a403c8c39c68ce6da7eac9c
kelv1n1n <48410278+kelv1n1n@users.noreply.github.com>
2024-10-24 10:49:20 +0800

    Update bilibili-ad.js

--- js/bilibili-ad.js
+++ js/bilibili-ad.js
@@ -46,7 +46,51 @@ if (url.includes("x/v2/splash")) {
       obj.data.top[0].pos = 1;
     }
   }
-}
+} else if (url.includes("/x/v2/account/mine?")) {
+  // 我的页面
+  const del = ["rework_v1", "vip_section", "vip_section_v2"];
+  for (let i of del) {
+    // 不必要项目
+    delete obj.data[i];
+  }
+  if (obj?.data?.sections_v2?.length > 0) {
+    let newSects = [];
+    for (let item of obj.data.sections_v2) {
+      delete item.button;
+      if (item?.style) {
+        if (item?.style === 1 || item?.style === 2) {
+          if (item?.title) {
+            if (item?.title === "创作中心" || item?.title === "推荐服务") {
+              // 创作中心 推荐服务
+              continue;
+            } else if (item?.title === "更多服务") {
+              delete item.title;
+              if (item?.items?.length > 0) {
+                let newItems = [];
+                for (let i of item.items) {
+                  if (/user_center\/feedback/g.test(i?.uri)) {
+                    // 联系客服
+                    newItems.push(i);
+                  } else if (/user_center\/setting/g.test(i?.uri)) {
+                    // 设置
+                    newItems.push(i);
+                  } else {
+                    continue;
+                  }
+                }
+                item.items = newItems;
+              }
+            }
+          }
+        } else {
+          // 其他style
+          continue;
+        }
+      }
+      newSects.push(item);
+    }
+    obj.data.sections_v2 = newSects;
+  }
 
 body = JSON.stringify(obj);
 $done({body});
