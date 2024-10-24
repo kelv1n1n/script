b10958235a2ce745e77eb068150d372ea51f1912
kelv1n1n <48410278+kelv1n1n@users.noreply.github.com>
2024-10-24 09:47:14 +0800

    Update bilibili-ad.js

--- js/bilibili-ad.js
+++ js/bilibili-ad.js
@@ -1,1 +1,14 @@
+let url = $request.url;
+let body = $response.body;
+let obj = JSON.parse(body);
 
+if (url.includes("x/v2/splash")) {
+    if (!obj.data.show) {
+      console.log('数据无show字段');
+    } else {
+      delete obj.data.show;
+    }
+}
+
+body = JSON.stringify(obj);
+$done({body});
