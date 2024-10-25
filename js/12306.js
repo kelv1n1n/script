091f8bb03dd15b1f72e653f89717394d486ae0c7
kelv1n1n <48410278+kelv1n1n@users.noreply.github.com>
2024-10-25 10:05:05 +0800

    Update 12306.js

--- js/12306.js
+++ js/12306.js
@@ -1,21 +1,18 @@
+// 2024-02-19 10:45
+
 let body = "";
 let obj = JSON.parse($request.body);
 const isQuanX = typeof $task !== "undefined";
-//console.log(isQuanX);
-console.log(JSON.stringify(obj.placementNo));
 
 if (obj.placementNo === "0007") {
   body =
-    '{"code":"00","materialsList":[{"billMaterialsId":"257","filePath":"h","creativeType":2}],"advertParam":{"showSkip":0}';
-  //body = '{"code":"00","materialsList":[]}';
+    '{"code":"00","materialsList":[{"billMaterialsId":"255","filePath":"h","creativeType":1}],"advertParam":{"skipTime":1}}';
 } else if (obj.placementNo === "G0054") {
   body = '{"code":"00","materialsList":[]}';
 } else {
   body = '{"code":"00","message":"无广告返回"}';
 }
-//body = '{"code":"00","message":"无广告返回"}';
 
-console.log(JSON.stringify(body));
 if (isQuanX) {
   $done({ body });
 } else {
