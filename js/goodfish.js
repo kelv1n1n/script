9e4da8687053cb108f59f062a04a8f5788c0e022
kelv1n1n <48410278+kelv1n1n@users.noreply.github.com>
2024-12-03 10:27:08 +0800

    Update goodfish.js

--- js/goodfish.js
+++ js/goodfish.js
@@ -5,7 +5,25 @@ let url = $request.url;
 let body = $response.body;
 let obj = JSON.parse(body);
 
-if (url.indexOf(user) != -1) {
+if (url.includes("/gw/mtop.taobao.idlehome.home.nextfresh")) {
+  // 可能存在的首页标签
+  delete obj.data.widgetReturnDO;
+  // 删除banner图
+  delete obj.data.bannerReturnDO;
+  // 信息流广告
+  if (obj.data?.sections) {
+    obj.data.sections = obj.data.sections.filter(section => {
+      return !(section.data && (section.data.bizType === "AD" || section.data.bizType === "homepage"));
+    });
+
+    let excludeNames = ['fish_home_yunying_card_d3', 'idlefish_seafood_market', 'fish_home_chat_room'];
+    obj.data.sections = obj.data.sections.filter(function(section) {  
+      return !excludeNames.includes(section.template.name);  
+    });
+  }
+}
+
+if (url.includes("/mtop.idle.user.page.my.adapter")) {
   //  底部社区小程序列表
   obj.data.container.sections = obj.data.container.sections.filter(item => item.index !== "6");
   //  个人主页横幅
