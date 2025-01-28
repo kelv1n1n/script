a5bed0c313a646cec9fdad32844c3a5e677678f9
kelv1n1n <48410278+kelv1n1n@users.noreply.github.com>
2025-01-28 09:34:42 +0800

    Update hp.js

--- js/hp.js
+++ js/hp.js
@@ -33,5 +33,42 @@ if (url.indexOf(liveTabList) != -1) {
  obj.result.categoryList = obj.result.categoryList.filter(item => !tabList.includes(item.categoryId));
 }
 
+
+
+
+//  -------------怎么肥事-----------------
+if (url.includes("/buffer/hotList")) {
+    delete obj.result.topBanner;
+}
+
+if (url.includes("/mang/preview/banners")) {
+    delete obj.data;
+}
+
+if (url.includes("/bbsallapi/lego/data")) {
+    obj.data.cards.forEach(card => { 
+        if (card.code === "multiIcon") {
+            const titlesToRemove = ["个性换肤", "专家预测", "邀请好友", "版主中心", "JRs战术板", "草稿箱" , "58同城"];
+            if (card.components && card.components.length > 0) {
+                card.components = card.components.filter(component => {
+                    return !(component.data && titlesToRemove.includes(component.data.title));
+                });
+            }
+        }
+    });
+}
+
+if (url.includes("/bplapi/user/v1/more")) {
+    if (obj.result && obj.result.vipInfo && obj.result.vipInfo.textInfo) {
+        delete obj.result.vipInfo.textInfo;
+    }
+    if (obj.result && obj.result.vipInfo) {
+        delete obj.result.vipInfo;
+    }
+}
+
+
+
+
 body = JSON.stringify(obj);
 $done({body});
