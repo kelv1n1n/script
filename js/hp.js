f367ed39effd8eaaa8e79a5bb49291f529fb1a15
kelv1n1n <48410278+kelv1n1n@users.noreply.github.com>
2025-01-28 09:49:26 +0800

    Update hp.js

--- js/hp.js
+++ js/hp.js
@@ -58,6 +58,16 @@ if (url.includes("/bbsallapi/lego/data")) {
     });
 }
 
+//   个人页我的应用菜单
+if (obj.data && obj.data.cards) {
+    obj.data.cards = obj.data.cards.filter(card => {
+        if (card.components && card.components.length > 0) {
+            return !card.components.some(component => component.data && component.data.title === "我的应用");
+        }
+        return true;
+    });
+}
+
 if (url.includes("/bplapi/user/v1/more")) {
     if (obj.result && obj.result.vipInfo && obj.result.vipInfo.textInfo) {
         delete obj.result.vipInfo.textInfo;
