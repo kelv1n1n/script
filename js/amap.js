a6c34a5a42901dcddf1fba9e62a292408047706f
kelv1n1n <48410278+kelv1n1n@users.noreply.github.com>
2025-03-14 08:49:03 +0800

    Update amap.js

--- js/amap.js
+++ js/amap.js
@@ -429,7 +429,8 @@ if (url.includes("/aos/perception/publicTravel/beforeNavi")) {
     "yellowPageAdRecommendModule", //淘宝商品推荐
     "similarShelfRecommend", //附近相似推荐
     "similarShopRecommend", //附近相似店铺推荐
-    "commonAiAgent" //Ai助手
+    "commonAiAgent", //Ai助手
+    "societyPublicExperience" //地图共建
   ];
   if (obj?.data?.modules) {
     for (let i of items) {
